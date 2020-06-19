ad66ea1aa7b81258e7b49291796c026372fd2001
modargo <modargo@gmail.com>
2020-06-19 11:52:40 -0400

    Add Three Animals event

--- src/main/java/menagerie/events/BeastSpiritShrine.java
+++ src/main/java/menagerie/events/BeastSpiritShrine.java
@@ -1,15 +1,12 @@
 package menagerie.events;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.localization.EventStrings;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
-import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 import menagerie.Menagerie;
 import menagerie.cards.BeastSpirit;
 
---
+++ src/main/java/menagerie/events/ThreeAnimals.java
@@ -0,0 +1,93 @@
+package menagerie.events;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.curses.Injury;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.AbstractEvent;
+import com.megacrit.cardcrawl.events.AbstractImageEvent;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.vfx.cardManip.PurgeCardEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import menagerie.Menagerie;
+import menagerie.cards.BeastSpirit;
+
+import java.text.MessageFormat;
+
+public class ThreeAnimals extends AbstractImageEvent {
+    public static final String ID = "Menagerie:ThreeAnimals";
+    private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
+    private static final String NAME = eventStrings.NAME;
+    private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
+    private static final String[] OPTIONS = eventStrings.OPTIONS;
+    public static final String IMG = Menagerie.eventImage(ID);
+    private static final float HEAL_PERCENT = 0.25f;
+    private static final float A15_HEAL_PERCENT = 0.20f;
+
+    private int healAmount;
+    private AbstractCard curse;
+    private int screenNum = 0;
+
+    public ThreeAnimals() {
+        super(NAME, DESCRIPTIONS[0], IMG);
+
+        this.curse = new Injury();
+        float healPercent = AbstractDungeon.ascensionLevel >= 15 ? A15_HEAL_PERCENT : HEAL_PERCENT;
+        this.healAmount = (int)(AbstractDungeon.player.maxHealth * healPercent);
+
+        imageEventText.setDialogOption(OPTIONS[0], this.curse);
+        imageEventText.setDialogOption(MessageFormat.format(OPTIONS[1], this.healAmount));
+        imageEventText.setDialogOption(OPTIONS[2]);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(0);
+            AbstractDungeon.topLevelEffects.add(new PurgeCardEffect(c, (float)(Settings.WIDTH / 2), (float)(Settings.HEIGHT / 2)));
+            AbstractDungeon.player.masterDeck.removeCard(c);
+            AbstractDungeon.gridSelectScreen.selectedCards.remove(c);
+        }
+    }
+
+    @Override
+    protected void buttonEffect(int buttonPressed) {
+        switch (screenNum) {
+            case 0:
+                switch (buttonPressed) {
+                    case 0: // Lion
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
+                        AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(this.curse, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
+                        AbstractRelic r = AbstractDungeon.returnRandomScreenlessRelic(AbstractDungeon.returnRandomRelicTier());
+                        AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float)(Settings.WIDTH / 2), (float)(Settings.HEIGHT / 2), r);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[3]);
+                        this.imageEventText.clearRemainingOptions();
+                        break;
+                    case 1: // Goat
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
+                        AbstractDungeon.player.heal(this.healAmount, true);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[3]);
+                        this.imageEventText.clearRemainingOptions();
+                        break;
+                    case 2: // Snake
+                        this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
+                        this.screenNum = 1;
+                        this.imageEventText.updateDialogOption(0, OPTIONS[3]);
+                        this.imageEventText.clearRemainingOptions();
+                        AbstractDungeon.gridSelectScreen.open(CardGroup.getGroupWithoutBottledCards(AbstractDungeon.player.masterDeck.getPurgeableCards()), 1, OPTIONS[4], false, false, false, true);
+                        break;
+                }
+                break;
+            default:
+                this.openMap();
+                break;
+        }
+    }
+}
