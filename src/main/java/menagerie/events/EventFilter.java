6af3ac7b6ba3295e4a50882bb68842b4dc51d711
modargo <modargo@gmail.com>
2020-06-26 02:37:24 -0400

    Change Mossy Path to only show up after floor 6, since it involves a strong fight

--- src/main/java/menagerie/events/EventFilter.java
+++ src/main/java/menagerie/events/EventFilter.java
@@ -18,6 +18,11 @@ public class EventFilter {
                     eventsToRemove.add(event);
                 }
             }
+            if (event.equals(MossyPath.ID)) {
+                if (!(AbstractDungeon.currMapNode.y > 6)) {
+                    eventsToRemove.add(event);
+                }
+            }
 
             if (event.equals(MagicPeddler.ID)) {
                 int goldRequired = AbstractDungeon.ascensionLevel >= 15 ? MagicPeddler.A15_POTION_COST : MagicPeddler.POTION_COST;
