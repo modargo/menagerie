902fc4289076f6ee80fa06c66bb04f724349506c
modargo <modargo@gmail.com>
2020-06-27 11:34:34 -0400

    Rework Tinker to exhaust a card (random is unupgraed) instead of giving -1 strength and dexterity

--- src/main/java/menagerie/cards/spells/Tinker.java
+++ src/main/java/menagerie/cards/spells/Tinker.java
@@ -2,6 +2,7 @@ package menagerie.cards.spells;
 
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -19,28 +20,29 @@ public class Tinker extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 2;
-    private static final int UPGRADE_COST = 1;
-    private static final int STATS = 1;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    private static final int COST = 1;
 
     public Tinker() {
         super(ID, NAME, IMG, COST, DESCRIPTION, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.SELF);
-        this.baseMagicNumber = STATS;
-        this.magicNumber = this.baseMagicNumber;
         this.exhaust = true;
         this.tags.add(CustomTags.GRAND_MAGUS_SPELL);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, -this.magicNumber), -this.magicNumber));
-        this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, -this.magicNumber), -this.magicNumber));
         this.addToBot(new TinkerAction());
+        if (this.upgraded) {
+            this.addToBot(new ExhaustAction(1, false));
+        } else {
+            this.addToBot(new ExhaustAction(1, true, false, false));
+        }
     }
 
     public void upgrade() {
         if (!this.upgraded) {
-            this.retain = true;
-            this.upgradeBaseCost(UPGRADE_COST);
+            this.upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
