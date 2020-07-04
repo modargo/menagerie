77fce8658a010b416ec400ed90f0a50629f7a3c0
modargo <modargo@gmail.com>
2020-07-04 15:30:11 -0400

    Fix Crumbling Sanctuary upgrade, which was unintentionally high

--- src/main/java/menagerie/cards/spells/CrumblingSanctuary.java
+++ src/main/java/menagerie/cards/spells/CrumblingSanctuary.java
@@ -19,7 +19,7 @@ public class CrumblingSanctuary extends CustomCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 3;
     public static final int DAMAGE_PER_CARD = 2;
-    public static final int UPGRADE_DAMAGE_PER_CARD = 3;
+    public static final int UPGRADE_DAMAGE_PER_CARD = 1;
 
     public CrumblingSanctuary() {
         super(ID, NAME, IMG, COST, DESCRIPTION, CardType.POWER, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.SELF);
