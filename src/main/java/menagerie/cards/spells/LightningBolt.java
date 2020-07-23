cee939af13b2321395853e071d9e6ff25cf94de1
modargo <modargo@gmail.com>
2020-07-23 18:20:59 -0400

    Change Lightning Bolt to attack a random enemy when exhausted

--- src/main/java/menagerie/cards/spells/LightningBolt.java
+++ src/main/java/menagerie/cards/spells/LightningBolt.java
@@ -3,6 +3,7 @@ package menagerie.cards.spells;
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -10,6 +11,7 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
@@ -44,6 +46,11 @@ public class LightningBolt extends CustomCard {
             this.upgradeName();
             this.upgradeDamage(UPGRADE_DAMAGE);
         }
+    }
+
+    @Override
+    public void triggerOnExhaust() {
+        this.addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.LIGHTNING));
     }
 
     public AbstractCard makeCopy() {
