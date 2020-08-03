e807dfbd719140496fc0a161a3d3867b67688639
modargo <modargo@gmail.com>
2020-08-02 22:55:13 -0400

    Reduce Slaughter damage by 1

--- src/main/java/menagerie/cards/Slaughter.java
+++ src/main/java/menagerie/cards/Slaughter.java
@@ -22,7 +22,7 @@ public class Slaughter extends CustomCard {
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 1;
-    private static final int DAMAGE = 12;
+    private static final int DAMAGE = 11;
     private static final int UPGRADE_DAMAGE = 2;
 
     public Slaughter() {
