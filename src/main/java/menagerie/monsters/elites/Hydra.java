eda917ddf1f0cda9a197c4508b14d233f3e0b8dc
modargo <modargo@gmail.com>
2020-08-21 14:58:34 -0400

    Increase Hydra HP slightly

--- src/main/java/menagerie/monsters/elites/Hydra.java
+++ src/main/java/menagerie/monsters/elites/Hydra.java
@@ -34,10 +34,10 @@ public class Hydra extends CustomMonster
     private static final int STRENGTH = 4;
     private static final int A3_STRENGTH = 5;
     private static final int HEAD_DAMAGE_THRESHOLD = 7;
-    private static final int HP_MIN = 83;
-    private static final int HP_MAX = 87;
-    private static final int A8_HP_MIN = 85;
-    private static final int A8_HP_MAX = 89;
+    private static final int HP_MIN = 87;
+    private static final int HP_MAX = 90;
+    private static final int A8_HP_MIN = 89;
+    private static final int A8_HP_MAX = 92;
     private int strength;
 
     public Hydra() {
