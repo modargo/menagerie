7f993e52d93020c79c43efbcca80de5c5e9c19af
modargo <modargo@gmail.com>
2020-12-04 01:56:40 -0500

    Update for compatibility with Slay the Spire 2.2, fixing game over screen crash

--- src/main/java/menagerie/patches/CreateGameOverStatsPatch.java
+++ src/main/java/menagerie/patches/CreateGameOverStatsPatch.java
@@ -1,12 +1,14 @@
 package menagerie.patches;
 
 import actlikeit.savefields.BreadCrumbs;
+import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.Exordium;
 import com.megacrit.cardcrawl.screens.DeathScreen;
+import com.megacrit.cardcrawl.screens.GameOverScreen;
 import com.megacrit.cardcrawl.screens.GameOverStat;
 import com.megacrit.cardcrawl.screens.VictoryScreen;
 
@@ -37,7 +39,8 @@ public class CreateGameOverStatsPatch {
     public static class VictoryScreenPatch {
         @SpirePostfixPatch
         public static void VictoryScreenPatch(VictoryScreen __instance) {
-            RemoveScoreEntries(__instance.stats);
+            ArrayList<GameOverStat> stats = (ArrayList<GameOverStat>) ReflectionHacks.getPrivate(__instance, GameOverScreen.class, "stats");
+            RemoveScoreEntries(stats);
         }
     }
 
@@ -48,7 +51,8 @@ public class CreateGameOverStatsPatch {
     public static class DeathScreenPatch {
         @SpirePostfixPatch
         public static void DeathScreenPatch(DeathScreen __instance) {
-            RemoveScoreEntries(__instance.stats);
+            ArrayList<GameOverStat> stats = (ArrayList<GameOverStat>) ReflectionHacks.getPrivate(__instance, GameOverScreen.class, "stats");
+            RemoveScoreEntries(stats);
         }
     }
 }
