ba55c774ce5c3d54f64d7f0dbca4e0dce64e3c1e
modargo <modargo@gmail.com>
2022-08-01 15:05:03 -0400

    Change code using unparameterized types and iterators to use parameterized types and for loops (mostly)

--- src/main/java/menagerie/actions/CardTypeDiscardPileToHandAction.java
+++ src/main/java/menagerie/actions/CardTypeDiscardPileToHandAction.java
@@ -13,7 +13,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.stream.Collectors;
 
 public class CardTypeDiscardPileToHandAction extends AbstractGameAction {
@@ -43,19 +42,9 @@ public class CardTypeDiscardPileToHandAction extends AbstractGameAction {
             logger.info("Returning " + this.numberOfCards + " card(s). Eligible cards: " + eligibleCards.group.stream().map(c -> c.cardID).collect(Collectors.joining(", ")));
             if (!eligibleCards.isEmpty() && this.numberOfCards > 0) {
                 if (eligibleCards.size() <= this.numberOfCards && !this.optional) {
-                    ArrayList<AbstractCard> cardsToMove = new ArrayList();
-                    Iterator var5 = eligibleCards.group.iterator();
+                    ArrayList<AbstractCard> cardsToMove = new ArrayList<>(eligibleCards.group);
 
-                    AbstractCard c;
-                    while(var5.hasNext()) {
-                        c = (AbstractCard)var5.next();
-                        cardsToMove.add(c);
-                    }
-
-                    var5 = cardsToMove.iterator();
-
-                    while(var5.hasNext()) {
-                        c = (AbstractCard)var5.next();
+                    for (AbstractCard c : cardsToMove) {
                         if (this.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
                             this.player.hand.addToHand(c);
                             if (this.setCost) {
@@ -90,13 +79,8 @@ public class CardTypeDiscardPileToHandAction extends AbstractGameAction {
                 this.isDone = true;
             }
         } else {
-            Iterator var1;
-            AbstractCard c;
             if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
-                var1 = AbstractDungeon.gridSelectScreen.selectedCards.iterator();
-
-                while(var1.hasNext()) {
-                    c = (AbstractCard)var1.next();
+                for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
                     if (this.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
                         this.player.hand.addToHand(c);
                         if (this.setCost) {
@@ -111,10 +95,10 @@ public class CardTypeDiscardPileToHandAction extends AbstractGameAction {
                     c.applyPowers();
                 }
 
-                for(var1 = this.player.discardPile.group.iterator(); var1.hasNext(); c.target_y = 0.0F) {
-                    c = (AbstractCard)var1.next();
+                for(AbstractCard c : this.player.discardPile.group) {
                     c.unhover();
                     c.target_x = (float)CardGroup.DISCARD_PILE_X;
+                    c.target_y = 0.0F;
                 }
 
                 AbstractDungeon.gridSelectScreen.selectedCards.clear();
@@ -123,10 +107,7 @@ public class CardTypeDiscardPileToHandAction extends AbstractGameAction {
 
             this.tickDuration();
             if (this.isDone) {
-                var1 = this.player.hand.group.iterator();
-
-                while(var1.hasNext()) {
-                    c = (AbstractCard)var1.next();
+                for (AbstractCard c : this.player.hand.group) {
                     c.applyPowers();
                 }
             }
