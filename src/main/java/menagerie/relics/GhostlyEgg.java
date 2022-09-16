cc82bc15fee1127df5c46e175f45c2a7cc3a6179
modargo <modargo@gmail.com>
2022-09-16 12:33:53 -0400

    Add null check to Ghostly Egg onEquip, to avoid issues if it somehow spawns outside its associated event

--- src/main/java/menagerie/relics/GhostlyEgg.java
+++ src/main/java/menagerie/relics/GhostlyEgg.java
@@ -22,8 +22,10 @@ public class GhostlyEgg extends CustomRelic {
     @Override
     public void onEquip() {
         for (RewardItem reward : AbstractDungeon.combatRewardScreen.rewards) {
-            for (AbstractCard c : reward.cards) {
-                this.onPreviewObtainCard(c);
+            if (reward.cards != null) {
+                for (AbstractCard c : reward.cards) {
+                    this.onPreviewObtainCard(c);
+                }
             }
         }
     }
